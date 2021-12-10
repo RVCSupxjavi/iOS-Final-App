503ebee32d659e80b3ff55128aaba837801afb08
Javier Jimenez Jr <s0360579@student.rockvalleycollege.edu>
2021-12-10 01:59:43 -0600

    Initial Commit

---
+++ finalappv2UITests/finalappv2UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  finalappv2UITestsLaunchTests.swift
+//  finalappv2UITests
+//
+//  Created by Javier Jimenez Jr on 12/10/21.
+//
+
+import XCTest
+
+class finalappv2UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
